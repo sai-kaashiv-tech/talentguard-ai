01c5a366c114d71577ab7213c29a82ad28458d67
saiaravind <saiad.kaashiv@gmail.com>
2026-07-02 16:52:08 +0530

    Data and first python file added

--- main.py
+++
@@ -1,3 +0,0 @@
-from src.new import func1
-print('hello , welcome to talentguard-AI')
-func1()
---
+++ src/main.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+data = pd.read_csv('Data/Raw/Employee-Attrition.csv')
+print(data.head())
